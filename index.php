b60dcd1b62699f3a4c45d9b7b67ac6c259817ff5
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2022-11-08 21:24:22 -0300

    first commit - php course

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+//Arquivo index responsável pela inicialização do sistema.
+//include → Inclui o que estiver no arquivo
+//require → Só funciona se estiver tudo corrreto com o arquivo "requerido"
+//require_once → requer apenas uma vez o arquivo solicitado
+
+require_once 'sistema/configuracao.php';
+include_once 'helpers.php';
+
+echo '<h1>Arquivo index</h1>';
+
+//Iniciarei na aula 13 do curso:
+//https://www.youtube.com/watch?v=dXXL8-Z5uXA&list=PL0N5TAOhX5E9eJ9Ix6YUIgEw3lNmaIEE9&index=12
