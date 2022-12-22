f1e18f6f7f5b4a146f8f5610550b315fdcd2d9cd
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2022-12-22 20:16:42 -0300

    introduction to the Magic construct

--- index.php
+++ index.php
@@ -14,16 +14,31 @@
 require_once 'sistema/configuracao.php';
 include_once 'sistema/Nucleo/Helpers.php';
 include './sistema/Nucleo/Mensagem.php';
+include './sistema/Nucleo/Controlador.php';
 
-use sistema\Nucleo\Helpers;
+use sistema\Nucleo\Controlador;
 
-// use sistema\Nucleo\Mensagem;
-// echo (new Mensagem())->alerta('Texto de alerta!');
+$controlador = new Controlador();
+echo '<hr>';
+var_dump($controlador);
 
-// echo Helpers::limparNumero('25ss993h658h4w125h1');
-echo Helpers::saudacao();
 
-echo '<br/>';
+
+
+
+
+
+
+
+// use sistema\Nucleo\Helpers;
+
+// // use sistema\Nucleo\Mensagem;
+// // echo (new Mensagem())->alerta('Texto de alerta!');
+
+// // echo Helpers::limparNumero('25ss993h658h4w125h1');
+// echo Helpers::saudacao();
+
+// echo '<br/>';
 
 
 // <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
