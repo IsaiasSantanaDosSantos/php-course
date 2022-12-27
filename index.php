35bc51f7824f5653a346dafffc2828797bb1b765
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2022-12-27 20:35:05 -0300

    install pecee/simple-router

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require 'vendor/autoload.php';
     
 echo SITE_NOME;
 
-echo sistema\Nucleo\Helpers::saudacao();
+// echo sistema\Nucleo\Helpers::saudacao();
 
 
 //next video: https://www.youtube.com/watch?v=nlR3blnlym8&list=PL0N5TAOhX5E9eJ9Ix6YUIgEw3lNmaIEE9&index=49
